42aeaf019bfb465cb3a4184cadc66bb31382bd14
Dhritiman Bharadwaj <dhritimanbhardwaj@gmail.com>
2022-04-11 17:14:31 +0530

    minor changes

--- Evaluation/serializers.py
+++ Evaluation/serializers.py
@@ -1,13 +1,24 @@
-from numpy import source
 from rest_framework import serializers
 
-from Examination.models import StudentAnswersheet
+from Examination.models import StudentAnswersheet,AnswerSheet
 from .models import MarksObtained
 
+
 class MarksObtainedSerializer(serializers.ModelSerializer):
-    answersheet = serializers.UUIDField(source="answersheet.qr_code")
+    answersheet = serializers.UUIDField(source="answersheet.answersheet.qr_code")
 
     class Meta:
         model = MarksObtained
         fields = "__all__"
-    
+
+    def create(self, validated_data):
+        ans_sheet = validated_data['answersheet']['answersheet']["qr_code"]
+        ans_sheet = AnswerSheet.objects.get(qr_code = ans_sheet)
+        obj = MarksObtained.objects.create(
+            answersheet=StudentAnswersheet.objects.get(
+                answersheet=ans_sheet),
+            marks=validated_data['marks'],
+            examiner=validated_data["examiner"]
+        )
+        obj.save()
+        return obj
