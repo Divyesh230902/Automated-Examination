a5919b2600cbd28e9cbc2f50fa86cc1a79f87059
Dhritiman Bharadwaj <dhritimanbhardwaj@gmail.com>
2022-04-11 12:48:50 +0530

    serializer created

--- Examination/serializers.py
+++ Examination/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from .models import StudentAnswersheet
+
+from Dtail.models import Student
+from .models import AnswerSheet, StudentAnswersheet
 
 class StudentAnswerSheetSerializer(serializers.ModelSerializer):
+    student = serializers.SlugRelatedField(slug_field='qr_code',queryset=Student.objects.all())
+    answersheet = serializers.SlugRelatedField(slug_field='qr_code',queryset=AnswerSheet.objects.all())
     class Meta:
         model = StudentAnswersheet
         fields = "__all__"
+    
+    
