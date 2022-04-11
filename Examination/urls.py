38f1cf35a3c5e7678cbd562c596d59935ca01058
Dhritiman Bharadwaj <dhritimanbhardwaj@gmail.com>
2022-04-11 12:45:23 +0530

    API VIew added

--- Examination/urls.py
+++ Examination/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
+from Examination.views import CheckInvigilator, StudentAnswersheetAPIview
+
 urlpatterns = [
-    
+    path("get-exam/",CheckInvigilator.as_view()),
+    path("add-student-answersheet/",StudentAnswersheetAPIview.as_view()),
 ]
--- Examination/views.py
+++ Examination/views.py
@@ -3,11 +3,33 @@ from rest_framework import status
 from rest_framework import generics
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from datetime import datetime
+
+from Examination.serializers import StudentAnswerSheetSerializer
+from .models import Invigilagtor
 
 # Create your views here.
-class StudentAnswersheetAPIview(generics.GenericAPIView):
-    pass 
+class StudentAnswersheetAPIview(generics.CreateAPIView):
+    # authentication_classes = [TokenAuthentication]
+    # permission_classes = [IsAuthenticated]
+    serializer_class = StudentAnswerSheetSerializer
+
 
 class CheckInvigilator(generics.GenericAPIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
+
+    def get(self,request):
+        today = datetime.now().date()
+        data = Invigilagtor.objects.filter(professor__user = request.user,exam__date = today)
+        if data.exists():
+            return Response(
+                {
+                    "exam_id":data[0].exam.id
+                },
+                status=status.HTTP_200_OK
+            )
+        else:
+            return Response({
+                "message":"No Exam Found"
+            },status=status.HTTP_404_NOT_FOUND)
